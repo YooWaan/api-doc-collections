c793af00cc894e479c602bfc59e93bc3bd55e622
YooWaan <rterunuma@gmail.com>
2020-11-03 23:19:23 +0900

    audit fix

--- build.sh
+++ build.sh
@@ -5,20 +5,18 @@ DIST_DIR=dist
 rm -rf $DIST_DIR ; mkdir -p $DIST_DIR
 
 # aglio
-aglio -i aglio/api.md -o $DIST_DIR/aglio-api.html
+npx aglio -i aglio/api.md -o $DIST_DIR/aglio-api.html
 
 # blueprint
 mkdir $DIST_DIR/bootprint
 cd bootprint && node build.js > swagger.yaml && cd ..
-bootprint openapi bootprint/swagger.yaml $DIST_DIR/bootprint
+npx bootprint openapi bootprint/swagger.yaml $DIST_DIR/bootprint
 
 # RAML
-raml2html raml/api.raml > dist/raml-api.html
+npx raml2html raml/api.raml > dist/raml-api.html
 
 # apidocs
 mkdir $DIST_DIR/apidoc
-apidoc -i apidoc -o $DIST_DIR/apidoc
+npx apidoc -i apidoc -o $DIST_DIR/apidoc
 
-
-
-# sphinx
+# TODO sphinx
